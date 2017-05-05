b41832e7a32ef4979574dd7ed52193febb4431f5
Aya Nakazawa <aya@ayatec.top>
2017-05-06 00:19:14 +0900

    Update function getString in DatePlus
    Change to call getDateString

--- js/myscript.js
+++ js/myscript.js
@@ -66,6 +66,9 @@ class DatePlus {
     this.date = new Date(_date);
   }
   getString(format = '%Y/%m/%d %H:%M:%S') {
+    return DatePlus.getDateString(this.date, format);
+  }
+  static getDateString(date = new Date(), format = '%Y/%m/%d %H:%M:%S') {
     // Dateオブジェクトからゼロ埋めした日付文字列を生成
     // format: '%Y/%m/%d %H:%M:%S'
     //  %Y: 年4桁
@@ -75,16 +78,6 @@ class DatePlus {
     //  %H: 時
     //  %M: 分
     //  %S: 秒
-    format = format.replace('%Y', ("000" + this.date.getFullYear()).slice(-4));
-    format = format.replace('%y', ("0" + this.date.getFullYear()).slice(-2));
-    format = format.replace('%m', ("0" + (this.date.getMonth() + 1)).slice(-2));
-    format = format.replace('%d', ("0" + this.date.getDate()).slice(-2));
-    format = format.replace('%H', ("0" + this.date.getHours()).slice(-2));
-    format = format.replace('%M', ("0" + this.date.getMinutes()).slice(-2));
-    format = format.replace('%S', ("0" + this.date.getSeconds()).slice(-2));
-    return format;
-  }
-  static getDateString(date = new Date(), format = '%Y/%m/%d %H:%M:%S') {
     format = format.replace('%Y', ("000" + date.getFullYear()).slice(-4));
     format = format.replace('%y', ("0" + date.getFullYear()).slice(-2));
     format = format.replace('%m', ("0" + (date.getMonth() + 1)).slice(-2));
