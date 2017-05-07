f8d87669e72e140139f3cd486676566a2972b48b
Aya Nakazawa <aya@ayatec.top>
2017-05-08 00:16:05 +0900

    Add some var in getSteamInventoryJson.rb

--- ruby/getSteamInventoryJson.rb
+++ ruby/getSteamInventoryJson.rb
@@ -1,6 +1,30 @@
 #!ruby
 print "Content-type: text/html\n\n";
 
+require 'cgi'
+
+objCgi = CGI.new
+
 result = 'false'
+
+_steamid = objCgi["steamid"]
+_appid = objCgi["appid"]
+
+steamInventoryJsonName = 'http://';
+steamInventoryJsonName += 'steamcommunity.com/profiles/';
+steamInventoryJsonName += "#{_steamid}/";
+steamInventoryJsonName += 'inventory/';
+steamInventoryJsonName += 'json/';
+steamInventoryJsonName += "#{_appid}/";
+steamInventoryJsonName += '2.php';
+
+saveDirectory = File.expand_path("..", Dir.pwd);
+saveDirectory += '/';
+saveDirectory += 'downloads/';
+saveDirectory += 'json';
+
+saveJsonName = saveDirectory;
+saveJsonName += '/';
+saveJsonName += "#{_steamid}_#{_appid}.json";
 
 puts result
