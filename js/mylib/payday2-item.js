45c6ffffdcdf9e1c2c4162020ec50a66261bd435
Aya Nakazawa <aya@ayatec.top>
2017-05-14 23:42:50 +0900

    Add RARITY & getRarityId in PAYDAY2

--- js/mylib/payday2-item.js
+++ js/mylib/payday2-item.js
@@ -1,3 +1,40 @@
 
 class PAYDAY2 {
+  static get RARITY() {
+    return [
+      {
+        name: 'Common',
+        color: '2360D8'
+      },
+      {
+        name: 'Uncommon',
+        color: '9900FF'
+      },
+      {
+        name: 'Rare',
+        color: 'FF00FF'
+      },
+      {
+        name: 'Epic',
+        color: 'FF0000'
+      },
+      {
+        name: 'Legendary',
+        color: 'ffff00'
+      },
+      {
+        name: 'Undefined',
+        color: '000000'
+      },
+    ]
+  }
+  
+  static getRarityId(_rarity = 'Common') {
+    for(let i = 0; i <= PAYDAY2.RARITY.length; i++) {
+      if (PAYDAY2.RARITY[i]['name'] == _rarity) {
+        return i;
+      }
+    }
+    return PAYDAY2.RARITY.length - 1;
+  }
 }
