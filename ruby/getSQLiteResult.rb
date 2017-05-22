0f0d6a6634062bbc21f083baf7724d17d2bee5e5
Aya Nakazawa <aya@ayatec.top>
2017-05-22 11:28:09 +0900

    Add comment in getSQLiteResult.rb

--- ruby/getSQLiteResult.rb
+++ ruby/getSQLiteResult.rb
@@ -10,10 +10,15 @@ _query = objCgi["query"] + ';'
 
 result = {}
 
+# データベースを開く
 db = SQLite3::Database.new("../db/payday2.sqlite3")
+
+# ハッシュで返るようにする
 db.results_as_hash = true
+# SQL実行
 db.execute(_query) do |row|
   result[row['name']] = row
 end
 
+# JSONで返す
 print result.to_json
